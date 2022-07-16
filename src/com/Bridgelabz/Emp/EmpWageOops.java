ddfb12fcd6c410a5c0c8155d343dfa7a96e9a55e
vishal5394 <vishaljadyal@gmail.com>
2022-07-16 17:54:51 +0530

    Added:Welcome to Employee wage

---
+++ src/com/Bridgelabz/Emp/EmpWageOops.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz.Emp;
+
+public class EmpWageOops {
+	void  Welcome(){
+		System.out.println("Welcome to Employee Wage Computation Program"); 
+	}
+
+	public static void main(String[] args) {
+		EmpWageOops obj = new EmpWageOops();
+		obj.Welcome();
+
+	}
+
+}
