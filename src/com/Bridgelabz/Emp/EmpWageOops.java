ee7e99dfb37909ee09491cb12a37702857ae55e2
vishal5394 <vishaljadyal@gmail.com>
2022-07-16 18:06:49 +0530

    Added: Employee Attendance

--- src/com/Bridgelabz/Emp/EmpWageOops.java
+++ src/com/Bridgelabz/Emp/EmpWageOops.java
@@ -4,10 +4,21 @@ public class EmpWageOops {
 	void  Welcome(){
 		System.out.println("Welcome to Employee Wage Computation Program"); 
 	}
+	void Attendance() {
+		int Is_Present = 1;
+		double Attendance = Math.floor(Math.random()*10)%2;
+		
+		if (Attendance == Is_Present)
+		System.out.println("Employee is Present");
+		
+		else
+			System.out.println("Employee is absent");
+	}
 
 	public static void main(String[] args) {
 		EmpWageOops obj = new EmpWageOops();
 		obj.Welcome();
+		obj.Attendance();
 
 	}
 
