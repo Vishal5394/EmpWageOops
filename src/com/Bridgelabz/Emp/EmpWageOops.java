b8847457c730ebcac26e5a61769e0fcd75437460
vishal5394 <vishaljadyal@gmail.com>
2022-07-16 18:27:58 +0530

    Added:Employee Daily Wage.

--- src/com/Bridgelabz/Emp/EmpWageOops.java
+++ src/com/Bridgelabz/Emp/EmpWageOops.java
@@ -1,24 +1,35 @@
 package com.Bridgelabz.Emp;
 
 public class EmpWageOops {
+	static int Is_Present = 1;
+	static int Emp_wage_per_hr = 20;
+
 	void  Welcome(){
 		System.out.println("Welcome to Employee Wage Computation Program"); 
 	}
-	void Attendance() {
-		int Is_Present = 1;
+	void Daily_Wage() {
+		int  Employeehr = 0 , Daily_Wage = 0;
+		
 		double Attendance = Math.floor(Math.random()*10)%2;
 		
-		if (Attendance == Is_Present)
-		System.out.println("Employee is Present");
+		if (Attendance == Is_Present) {
+			Employeehr = 8;
+		System.out.println("Employee is Present ");
+		}
+		else {
+			Employeehr = 0;
+		System.out.println("Employee is Absent ");
+		}
+			
+		Daily_Wage = Emp_wage_per_hr  *Employeehr;
 		
-		else
-			System.out.println("Employee is absent");
+		System.out.println("Employee Daily Wage is = "+Daily_Wage);
 	}
 
 	public static void main(String[] args) {
 		EmpWageOops obj = new EmpWageOops();
 		obj.Welcome();
-		obj.Attendance();
+		obj.Daily_Wage();
 
 	}
 
